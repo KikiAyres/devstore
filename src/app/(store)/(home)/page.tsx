34c013c6820eda6fcc1f3a97ca4ff92986eef5c4
kiki <cristine_ayres@yahoo.com.br>
2024-04-15 14:22:01 -0300

    create store

---
+++ src/app/(store)/(home)/page.tsx
@@ -0,0 +1,89 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60,
+    },
+  })
+
+  const products = await response.json()
+
+  return products
+}
+
+export const metadata: Metadata = {
+  title: 'Home',
+}
+
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const [highLightedProduct, ...otherProducts] = await getFeaturedProducts()
+
+  return (
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6 mt-5">
+      <Link
+        href={`/product/${highLightedProduct.slug}`}
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-100/30 overflow-hidden flex justify-center"
+      >
+        <Image
+          id="cd"
+          src={highLightedProduct.image}
+          className="group-hover:scale-105 transition-transform duration-500"
+          width={920}
+          height={920}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2  border-zinc-300/30 bg-zinc-400/60 p-1 pl-5">
+          <span className="text-sm font-semibold truncate">{highLightedProduct.title}</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-gradient-to-br from-cyan-500 via-purple-500 to-pink-500 px-4 font-semibold">
+            {highLightedProduct.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
+          </span>
+        </div>
+      </Link>
+
+      {otherProducts.map((product) => {
+        return (
+          <Link
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative flex-col col-span-3 row-span-3 rounded-lg bg-zinc-100/30 overflow-hidden flex justify-center items-end"
+          >
+            <Image
+              src={product.image}
+              className="group-hover:scale-105 transition-transform duration-500"
+              width={920}
+              height={920}
+              quality={100}
+              alt=""
+            />
+
+            <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2  border-zinc-300/30 bg-zinc-400/60 p-1 pl-5">
+              <span className="text-sm font-semibold truncate">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-gradient-to-br from-cyan-500 via-purple-500 to-pink-500 px-4 font-semibold">
+                {product.price.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}
+              </span>
+            </div>
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
---
+++ src/app/(store)/product/[slug]/page.tsx
@@ -0,0 +1,123 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
+import { Metadata } from "next";
+import Image from "next/image";
+
+interface ProductProps {
+  params: {
+    slug: string
+  }
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60,
+    },
+  })
+
+  const products = await response.json()
+
+  return products
+}
+
+export async function generateMetadata({
+  params,
+  }:ProductProps): Promise<Metadata> {
+    const product = await getProduct(params.slug)
+
+    return {
+      title: product.title,
+    }
+}
+
+/*export async function generateStaticParams() {
+  const response = await api('/products/feature')
+  const products: Product[] = await response.json()
+   return products.map((product) => {
+    return { slug: product.slug}
+  })
+}*/
+
+export default async function ProductPage({ params }: ProductProps) {
+  const product = await getProduct(params.slug)
+
+  return (
+  <div className="relative grid max-h-[860px] grid-cols-3">
+    <div className="col-span-2 overflow-hidden">
+      <Image
+        src={product.image}
+        alt=""
+        width={1000}
+        height={1000}
+        quality={100}
+      />
+    </div>
+
+    <div className="flex flex-col justify-center px-12">
+      <h1 className="text-3xl font-bold leading-tight">
+        {product.title}
+      </h1>
+
+      <p className="mt-2 leading-relaxed text-zinc-700">
+      {product.description}
+      </p>
+
+      <div className="mt-8 flex items-center gap-3">
+        <span className="inline-block rounded-full bg-zinc-300 px-5 py-2.5 text-zinc-700 font-semibold">
+          {product.price.toLocaleString('pt-BR',{
+          style:'currency',
+          currency:'BRL',
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+          })}
+        </span>
+        <span className="text-sm font-semibold text-white">
+          {(product.price / 12).toLocaleString('pt-BR',{
+          style:'currency',
+          currency:'BRL',          
+          })}
+        </span>
+      </div>
+
+      <div className="mt-8 space-y-4">
+        <span className="block font-semibold">
+          Tamanhos
+        </span>
+
+        <div className="flex gap-2 ">
+          <button
+           type="button"
+           className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-500 bg-zinc-500/50 text-sm font-semibold">
+            P
+           </button>
+
+           <button
+           type="button"
+           className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-500 bg-zinc-500/50 text-sm font-semibold">
+            M
+           </button>
+
+           <button
+           type="button"
+           className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-500 bg-zinc-500/50 text-sm font-semibold">
+            G
+           </button>
+
+           <button
+           type="button"
+           className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-500 bg-zinc-500/50 text-sm font-semibold">
+            GG
+           </button>
+        </div>
+      </div>
+
+        <button 
+        type="button" 
+        className="mt-8 flex h-12 items-center justify-center rounded-full bg-cyan-500/40 font-semibold text-white">
+          Adicionar ao carrinho
+        </button>
+
+    </div>
+  </div>)
+}
